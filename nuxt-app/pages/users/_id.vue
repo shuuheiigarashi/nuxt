dbcf84a7325fe606e804bfdad6c5cb5f6da76aca
shuuheiigarashi <shuuhei.igarashi0618@gmail.com>
2021-01-11 23:58:59 +0900

    update

--- nuxt-app/pages/users/_id.vue
+++ nuxt-app/pages/users/_id.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <p>/users/_id.vue</p>
+    <!-- <p>/users/_id.vue</p> -->
+    <p>User ID: {{userId}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id
+    };
+  }
+};
+</script>
