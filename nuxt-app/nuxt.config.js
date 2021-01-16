aed40dee70169e785f41e95764cef996496822b8
shuuheiigarashi <shuuhei.igarashi0618@gmail.com>
2021-01-16 23:56:35 +0900

    update

--- nuxt-app/nuxt.config.js
+++ nuxt-app/nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: "nuxt-app",
+    titleTemplate: "%s | Nuxt.js tag items viewer",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
