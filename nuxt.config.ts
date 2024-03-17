25ff8375f8cfcba0ab8ff45e266f91ef03f8be32
Shuuhei Igarashi <shuuheiigarashi@USERnoMacBook-Pro.local>
2024-03-17 16:54:40 +0900

    microCMSに必要なモジュールをインストールする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-microcms-module"],
+  microCMS: {
+    apiKey: process.env.MICROCMS_API_KEY,
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  },
+});
