d12cecd1871fdffb661d4580dfbc7caee4f6a07b
Shuuhei Igarashi <shuuheiigarashi@USERnoMacBook-Pro.local>
2024-03-24 20:32:39 +0900

    検索のコンポーネントを用意する

---
+++ components/molecules/SearchForm.vue
@@ -0,0 +1,12 @@
+<!-- components/molecules/SearchForm.vue -->
+<template>
+  <div class="flex">
+    <Input />
+    <Button />
+  </div>
+</template>
+
+<script setup>
+import Input from "@/components/atoms/Input.vue";
+import Button from "@/components/atoms/Button.vue";
+</script>
